1be7052678a34626e67979d77aeac0056e18d107
Martin <martinmmbote@gmail.com>
2023-12-08 17:23:29 +0300

    grades animated

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const chooseGradeSection = document.getElementById('choose-grade-section');
 const homeSection = document.getElementById('home-section');
 const blackboardSection = document.getElementById('blackboard-section');
 const preschoolButton = document.getElementById('preschool-button');
+const kindergartenButton = document.getElementById('kindergarten-button');
+const gradeschoolButton = document.getElementById('gradeschool-button');
 
 
 
@@ -29,16 +31,78 @@ function backToHomeButton() {
     chooseGradeSection.style.pointerEvents = 'none';
 }
 
+
+
+//PreSchool Button
 preschoolButton.addEventListener('mouseover', function() {
     preschoolButton.style.marginTop = '0.15vw';
     preschoolButton.style.border = '0.2vw solid rgba(255, 255, 255, 1)';
     preschoolButton.style.backgroundColor = 'rgba(123, 123, 123, 0.9)';
+    kindergartenButton.style.marginLeft = '1.6vw';
+    gradeschoolButton.style.marginRight = '0.15vw';
+    kindergartenButton.style.opacity = '0.4';
+    gradeschoolButton.style.opacity = '0.4';
 });
 
 preschoolButton.addEventListener('mouseout', function() {
     preschoolButton.style.marginTop = '1.3vw';
     preschoolButton.style.border = '0.01vw solid rgba(255, 255, 255, 0.5)';
     preschoolButton.style.backgroundColor = 'rgba(123, 123, 123, 0.4)';
+    kindergartenButton.style.marginLeft = '0vw';
+    gradeschoolButton.style.marginRight = '0.7vw';
+    kindergartenButton.style.opacity = '1';
+    gradeschoolButton.style.opacity = '1';
+});
+
+
+
+//Kindergarten Button
+kindergartenButton.addEventListener('mouseover', function() {
+    kindergartenButton.style.marginTop = '0.15vw';
+    kindergartenButton.style.border = '0.2vw solid rgba(255, 255, 255, 1)';
+    kindergartenButton.style.backgroundColor = 'rgba(123, 123, 123, 0.9)';
+    preschoolButton.style.marginLeft = '0.15vw';
+    gradeschoolButton.style.marginRight = '0.15vw';
+    gradeschoolButton.style.opacity = '0.4';
+    preschoolButton.style.opacity = '0.4';
+});
+
+kindergartenButton.addEventListener('mouseout', function() {
+    kindergartenButton.style.marginTop = '1.3vw';
+    kindergartenButton.style.border = '0.01vw solid rgba(255, 255, 255, 0.5)';
+    kindergartenButton.style.backgroundColor = 'rgba(123, 123, 123, 0.4)';
+    preschoolButton.style.marginLeft = '0.7vw';
+    gradeschoolButton.style.marginRight = '0.7vw';
+    gradeschoolButton.style.opacity = '1';
+    preschoolButton.style.opacity = '1';
+});
+
+
+
+
+//Grade School Button
+gradeschoolButton.addEventListener('mouseover', function() {
+    gradeschoolButton.style.marginTop = '0.15vw';
+    gradeschoolButton.style.border = '0.2vw solid rgba(255, 255, 255, 1)';
+    gradeschoolButton.style.backgroundColor = 'rgba(123, 123, 123, 0.9)';
+    preschoolButton.style.marginLeft = '0.15vw';
+    kindergartenButton.style.marginLeft = '-1.6vw';
+    preschoolButton.style.opacity = '0.4';
+    kindergartenButton.style.opacity = '0.4';
+    // gradeschoolButton.style.marginRight = '0.15vw';
+    // gradeschoolButton.style.opacity = '0.4';
+});
+
+gradeschoolButton.addEventListener('mouseout', function() {
+    gradeschoolButton.style.marginTop = '1.3vw';
+    gradeschoolButton.style.border = '0.01vw solid rgba(255, 255, 255, 0.5)';
+    gradeschoolButton.style.backgroundColor = 'rgba(123, 123, 123, 0.4)';
+    preschoolButton.style.marginLeft = '0.7vw';
+    kindergartenButton.style.marginLeft = '0vw';
+    preschoolButton.style.opacity = '1';
+    kindergartenButton.style.opacity = '1';
+    // gradeschoolButton.style.marginRight = '0.7vw';
+    // gradeschoolButton.style.opacity = '1';
 });
 
 //mouseover event listener (start) to amongUsButton
